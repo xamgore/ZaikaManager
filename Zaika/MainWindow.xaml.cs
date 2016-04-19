ba05de18f25e1da94b65e60eca5e30f6c4e96fa9
xamgore <xamgore@ya.ru>
2016-04-19 21:40:42 +0300

    Refactor main window code

--- Zaika/MainWindow.xaml.cs
+++ Zaika/MainWindow.xaml.cs
@@ -9,47 +9,52 @@ using FlickrNet;
 
 namespace Zaika {
     public partial class MainWindow {
+        private static readonly Flickr Flickr = new Flickr("ce28f896e78baffae502ff23e1df8645", "e4bc6d42f6c0b074");
+
         private string databaseLogin;
         private string databasePassword;
 
-        private readonly List<StackPanel> toy;
-        private readonly List<string> toys;
-
         public MainWindow(string databaseLogin = "kek", string databasePassword = "") {
+            InitializeComponent();
+            DisplayProducts();
+
             this.databasePassword = databasePassword;
             this.databaseLogin = databaseLogin;
-            InitializeComponent();
+        }
 
-            toys = new List<string>(new[] { "fox", "elephant", "owl" });
-            toy = new List<StackPanel>();
-            products.ItemsSource = toy;
-            products.BorderThickness = new Thickness(2, 2, 2, 2);
+        public void DisplayProducts() {
+            var toyNames = new[] { "fox", "elephant", "owl" };
 
+            var toyList = new List<StackPanel>();
+            products.ItemsSource = toyList;
 
-            foreach (var toyName in toys) {
-                var tb = new TextBlock();
-                var im = new Image {
-                    Width = 50,
-                    Height = 50,
+            foreach (var name in toyNames) {
+                var icon = new Image {
                     Stretch = Stretch.UniformToFill,
+                    Width = 50,
+                    Height = 50
                 };
 
-                var stp = new StackPanel();
-                tb.Text = toyName;
-                var fl = new Flickr("ce28f896e78baffae502ff23e1df8645", "e4bc6d42f6c0b074");
-                var options = new PhotoSearchOptions { Tags = tb.Text + " toy", PerPage = 1, Page = 1 };
-                fl.PhotosSearchAsync(options, photos => {
-                    if (!photos.HasError)
-                        im.Source = new BitmapImage(new Uri(photos.Result.First().SmallUrl));
-                });
+                Flickr.PhotosSearchAsync(
+                    new PhotoSearchOptions { Tags = name + " toy", PerPage = 1, Page = 1 },
+                    photos => {
+                        if (!photos.HasError)
+                            icon.Source = new BitmapImage(new Uri(photos.Result.First().SmallUrl));
+                    });
 
-                stp.Children.Add(im);
-                stp.Children.Add(tb);
-                stp.Height = 50;
-                stp.Orientation = Orientation.Horizontal;
-                tb.Margin = new Thickness(10, 17, 0, 10);
-                tb.FontSize = 18;
-                toy.Add(stp);
+                var description = new TextBlock {
+                    Margin = new Thickness(10, 17, 0, 10),
+                    Text = name,
+                    FontSize = 18
+                };
+
+                var stp = new StackPanel {
+                    Orientation = Orientation.Horizontal,
+                    Children = { icon, description },
+                    Height = 50
+                };
+
+                toyList.Add(stp);
             }
         }
     }
