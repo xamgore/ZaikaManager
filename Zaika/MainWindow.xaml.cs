f0b1b34a4e1cbf906fb36f56dc3902bd3863633f
xamgore <xamgore@ya.ru>
2016-04-19 21:25:01 +0300

    Add async load of images from Flickr

--- Zaika/MainWindow.xaml.cs
+++ Zaika/MainWindow.xaml.cs
@@ -1,33 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using FlickrNet;
 
-namespace Zaika
-{
-    /// <summary>
-    /// Логика взаимодействия для Window1.xaml
-    /// </summary>
-    public partial class MainWindow
-    {
-        List<string> toys;
-        List<StackPanel> toy;
+namespace Zaika {
+    public partial class MainWindow {
+        private string databaseLogin;
+        private string databasePassword;
 
-        string databaseLogin;
-        string databasePassword;
-        
-        public MainWindow(string databaseLogin = "kek", string databasePassword = "")
-        {
+        private readonly List<StackPanel> toy;
+        private readonly List<string> toys;
+
+        public MainWindow(string databaseLogin = "kek", string databasePassword = "") {
             this.databasePassword = databasePassword;
             this.databaseLogin = databaseLogin;
             InitializeComponent();
@@ -36,25 +24,25 @@ namespace Zaika
             toy = new List<StackPanel>();
             products.ItemsSource = toy;
             products.BorderThickness = new Thickness(2, 2, 2, 2);
-           
 
-            for (int i = 0; i < toys.Count; i++)
-            {
-                TextBlock tb = new TextBlock();
-                Image im = new Image();
-                im.Width = 50;
-                im.Height = 50;
-                
-                StackPanel stp = new StackPanel();
-                tb.Text = toys[i];
-                try
-                {
-                    FlickrNet.Flickr fl = new FlickrNet.Flickr("ce28f896e78baffae502ff23e1df8645", "e4bc6d42f6c0b074");
+
+            foreach (var toyName in toys) {
+                var tb = new TextBlock();
+                var im = new Image {
+                    Width = 50,
+                    Height = 50
+                };
+
+                var stp = new StackPanel();
+                tb.Text = toyName;
+                try {
+                    var fl = new Flickr("ce28f896e78baffae502ff23e1df8645", "e4bc6d42f6c0b074");
                     var options = new PhotoSearchOptions { Tags = tb.Text + " toy", PerPage = 1, Page = 1 };
-                    PhotoCollection photos = fl.PhotosSearch(options);
-                    im.Source = new BitmapImage(new Uri(photos.First().SmallUrl));
-                }
-                catch { }
+                    fl.PhotosSearchAsync(options, photos => {
+                        im.Source = new BitmapImage(new Uri(photos.Result.First().SmallUrl));
+                    });
+                } catch { }
+
                 stp.Children.Add(im);
                 stp.Children.Add(tb);
                 stp.Height = 50;
