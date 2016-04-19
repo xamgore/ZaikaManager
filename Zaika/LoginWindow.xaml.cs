49a4126fad67813f31577ad2c0bde4bbb1e8080a
xamgore <xamgore@ya.ru>
2016-04-19 21:16:32 +0300

    Rewrite login window events

--- Zaika/LoginWindow.xaml.cs
+++ Zaika/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Zaika {
@@ -6,6 +7,13 @@ namespace Zaika {
         public LoginWindow() {
             InitializeComponent();
             Login.Focus();
+
+            KeyDown += (o, e) => {
+                if (e.Key == Key.Escape)
+                    Close();
+                else if (e.Key == Key.Enter)
+                    TryToSignIn(o, e);
+            };
         }
 
         private void TryToSignIn(object sender, RoutedEventArgs e) {
@@ -14,23 +22,11 @@ namespace Zaika {
             else if (Password.Password.Length == 0)
                 Password.Focus();
             else {
-                // todo: подключение к базе данных
+                // todo: connect to database
                 new MainWindow(Login.Text, Password.Password).Show();
                 Close();
             }
         }
 
-        private void LoginKeyDown(object sender, KeyEventArgs e) {
-            if (e.Key == Key.Enter && Login.Text.Trim().Length != 0)
-                Password.Focus();
-        }
-
-        private void PasswordKeyDown(object sender, KeyEventArgs e) {
-            if (e.Key != Key.Enter) return;
-
-            if (Login.Text.Trim().Length == 0)
-                Login.Focus();
-            else TryToSignIn(sender, e);
-        }
     }
 }
