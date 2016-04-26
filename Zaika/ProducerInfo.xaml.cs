af1893f8bb47ea140a61aefca7147b1197f2db6d
xamgore <xamgore@ya.ru>
2016-04-26 23:44:36 +0300

    Reformat code

--- Zaika/ProducerInfo.xaml.cs
+++ Zaika/ProducerInfo.xaml.cs
@@ -1,27 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Zaika
-{
-    /// <summary>
-    /// Логика взаимодействия для ProducerInfo.xaml
-    /// </summary>
-    public partial class ProducerInfo : UserControl
-    {
-        public ProducerInfo()
-        {
+﻿namespace Zaika {
+    public partial class ProducerInfo {
+        public ProducerInfo() {
             InitializeComponent();
         }
     }
