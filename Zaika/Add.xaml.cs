665cfce563ee499d2e5175756f8cb97ac632565c
xamgore <xamgore@ya.ru>
2016-05-26 10:00:44 +0300

    Show available amount of product in some warehouse

--- Zaika/Add.xaml.cs
+++ Zaika/Add.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Controls;
 using System.Linq;
 using System.Windows;
 using System.Windows.Threading;
@@ -8,6 +9,8 @@ using Zaika.Core;
 
 namespace Zaika {
     public partial class AddWindow {
+        private static int Amount = 0;
+
         public AddWindow() {
             InitializeComponent();
             KeyDown += (o, e) => { if (e.Key == Key.Enter) button_Click(o, null); };
@@ -16,9 +19,28 @@ namespace Zaika {
             DisplayProducers();
             DisplayWarehouses();
 
+            Products.SelectionChanged += LoadAvailableProductAmount;
+            Warehouses.SelectionChanged += LoadAvailableProductAmount;
+
             //DB.WarehousesLoaded += Ui(DisplayWarehouses);
             //DB.ProducersLoaded += Ui(DisplayProducers);
             //DB.ProductsLoaded += Ui(DisplayProducts);
+        }
+
+        private async void LoadAvailableProductAmount(object sender, SelectionChangedEventArgs e) {
+            if (Products.SelectedIndex == -1 || Warehouses.SelectedIndex == -1)
+                return;
+
+            var product = (Products.SelectedItem as ProductInfo).Product.Id;
+            var warehouse = DB.Warehouses[Warehouses.SelectedItem as string].Id;
+            button.IsEnabled = false;
+
+            await DB.Zaika.SingleAsync<Stuff>(MicroLite.Builder.SqlBuilder.Select("Amount").From("Stuffs")
+                  .Where("ProductId").IsEqualTo(product).AndWhere("WarehouseId").IsEqualTo(warehouse).ToSqlQuery())
+                .ContinueWith(task => Amount = task.Result?.Amount ?? 0);
+
+            maxAmount.Text = $"Доступно {Amount}";
+            button.IsEnabled = true;
         }
 
         private EventHandler Ui(Action action) =>
@@ -35,13 +57,16 @@ namespace Zaika {
         public void DisplayWarehouses() =>
            Warehouses.ItemsSource = DB.Warehouses.Keys.ToList();
 
-        private void button_Click(object sender, RoutedEventArgs e) {
+        private async void button_Click(object sender, RoutedEventArgs e) {
             int augment;
             decimal price;
 
             if (!int.TryParse(Augment.Text, out augment) ||
                 !decimal.TryParse(Price.Text, out price))
                 return;
+
+            var product = (Products.SelectedItem as ProductInfo).Product.Id;
+            var warehouse = DB.Warehouses[Warehouses.SelectedItem as string].Id;
 
             var op = new Operation {
                 Augment = augment,
@@ -52,7 +77,14 @@ namespace Zaika {
                 ProductId = (Products.SelectedItem as ProductInfo).Product.Id,
             };
 
-            DB.Zaika.InsertAsync(op).ContinueWith(_ => DB.InsertOperation(op));
+            await DB.Zaika.SingleAsync<Stuff>(MicroLite.Builder.SqlBuilder.Select("Amount").From("Stuffs")
+                  .Where("ProductId").IsEqualTo(product).AndWhere("WarehouseId").IsEqualTo(warehouse).ToSqlQuery())
+                .ContinueWith(task => Amount = task.Result.Amount);
+
+            if (Amount + augment < 0)
+                return;
+
+            await DB.Zaika.InsertAsync(op).ContinueWith(_ => DB.InsertOperation(op));
             Close();
         }
     }
